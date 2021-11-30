ce48eae114d6a1d226166832a618a78abbc7790c
natrajsk007 <94169022+natrajsk007@users.noreply.github.com>
2021-11-30 21:47:50 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,1 +1,17 @@
+/**
+ * @file feature.h
+ * @author Natarajan K
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+
+#include <avr/io.h>
+#include <util/delay.h>
 
